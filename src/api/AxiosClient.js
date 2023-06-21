8ff69c72f3893cf4477cdd19ab688788271dd271
thuanlevan72 <88273694+thuanlevan72@users.noreply.github.com>
2023-06-21 15:30:16 +0700

    feature connect data hosting

--- src/api/AxiosClient.js
+++ src/api/AxiosClient.js
@@ -4,7 +4,7 @@ import queryString from "query-string";
 // Set up default config for http requests here
 // Please have a look at here `https://github.com/axios/axios#request- config` for the full list of configs
 const axiosClient = axios.create({
-  baseURL: "https://localhost:7064/api/",
+  baseURL: "https://api.polyfood.store/api/",
   headers: {
     "content-type": "application/json",
   },
