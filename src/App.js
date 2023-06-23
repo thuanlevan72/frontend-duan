a63734039ff5a3582f00287fdc9b1c9a4065e8fa
thuanlevan72 <88273694+thuanlevan72@users.noreply.github.com>
2023-06-23 16:07:54 +0700

    Feature layout news

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import PropTypes from "prop-types";
 import React, { useEffect, Suspense, lazy, useState } from "react";
 import ScrollToTop from "./helpers/scroll-top";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Redirect,
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
 } from "react-router-dom";
 import { ToastProvider, useToasts } from "react-toast-notifications";
 import { multilanguage, loadLanguages } from "redux-multilanguage";
@@ -22,6 +22,9 @@ const ShopGridStandard = lazy(() => import("./pages/shop/ShopGridStandard"));
 const Product = lazy(() => import("./pages/shop-product/Product"));
 // blog pages
 const BlogStandard = lazy(() => import("./pages/blog/BlogStandard"));
+const BlogDetailsStandard = lazy(() =>
+  import("./pages/blog/BlogDetailsStandard")
+);
 // other pages
 const About = lazy(() => import("./pages/other/About"));
 const Contact = lazy(() => import("./pages/other/Contact"));
@@ -35,143 +38,126 @@ const CompleteOrder = lazy(() => import("./pages/other/CompleteOrder"));
 const NotFound = lazy(() => import("./pages/other/NotFound"));
 
 const App = (props) => {
-    const [messageApi, contextHolder] = message.useMessage();
-    useEffect(() => {
-        props.dispatch(
-            loadLanguages({
-                languages: {
-                    en: require("./translations/english.json"),
-                    fn: require("./translations/french.json"),
-                    de: require("./translations/germany.json"),
-                    vi: require("./translations/vietnames.json"),
-                },
-            })
-        );
-    });
-    return (
-        <ToastProvider placement="bottom-left">
-            <BreadcrumbsProvider>
-                <Router>
-                    <ScrollToTop messageApi={messageApi}>
-                        <Suspense
-                            fallback={
-                                <div className="flone-preloader-wrapper">
-                                    <div className="flone-preloader">
-                                        <span></span>
-                                        <span></span>
-                                    </div>
-                                </div>
-                            }
-                        >
-                            {contextHolder}
-                            <Switch>
-                                <Route
-                                    exact
-                                    path={process.env.PUBLIC_URL + "/"}
-                                    component={HomeOrganicFood}
-                                />
-                                {/* Homepages */}
-                                <Route
-                                    path={
-                                        process.env.PUBLIC_URL +
-                                        "/home-organic-food"
-                                    }
-                                    component={HomeOrganicFood}
-                                />
-                                {/* Shop pages */}
-                                <Route
-                                    path={
-                                        process.env.PUBLIC_URL +
-                                        "/shop-grid-standard"
-                                    }
-                                    component={ShopGridStandard}
-                                />
-                                {/* Shop product pages */}
-                                <Route
-                                    path={
-                                        process.env.PUBLIC_URL + "/product/:id"
-                                    }
-                                    render={(routeProps) => (
-                                        <Product
-                                            {...routeProps}
-                                            key={routeProps.match.params.id}
-                                        />
-                                    )}
-                                />
-                                {/* Blog pages */}
-                                <Route
-                                    path={
-                                        process.env.PUBLIC_URL +
-                                        "/blog-standard"
-                                    }
-                                    component={BlogStandard}
-                                />
-                                {/* Other pages */}
-                                <Route
-                                    path={process.env.PUBLIC_URL + "/about"}
-                                    component={About}
-                                />
-                                <Route
-                                    path={process.env.PUBLIC_URL + "/contact"}
-                                    component={Contact}
-                                />
-                                <Route
-                                    path={
-                                        process.env.PUBLIC_URL + "/my-account"
-                                    }
-                                    component={MyAccount}
-                                />
-                                <Route
-                                    path={
-                                        process.env.PUBLIC_URL +
-                                        "/login-register"
-                                    }
-                                    component={LoginRegister}
-                                />
-                                <Route
-                                    path={process.env.PUBLIC_URL + "/cart"}
-                                    component={Cart}
-                                />
-                                <Route
-                                    path={process.env.PUBLIC_URL + "/wishlist"}
-                                    component={Wishlist}
-                                />
-                                <Route
-                                    path={process.env.PUBLIC_URL + "/compare"}
-                                    component={Compare}
-                                />
-                                <Route
-                                    path={process.env.PUBLIC_URL + "/checkout"}
-                                    component={Checkout}
-                                />
-                                <Route
-                                    path={process.env.PUBLIC_URL + "/Complete"}
-                                    component={CompleteOrder}
-                                />
-                                <Route
-                                    path={process.env.PUBLIC_URL + "/not-found"}
-                                    component={NotFound}
-                                />
-                                {/* Router Admin Manager */}
-                                <Route
-                                    path="/admin"
-                                    component={AdminLayout}
-                                    render={() => (
-                                        <Redirect to="/admin/dashboard" />
-                                    )}
-                                />
-                                <Route exact component={NotFound} />
-                            </Switch>
-                        </Suspense>
-                    </ScrollToTop>
-                </Router>
-            </BreadcrumbsProvider>
-        </ToastProvider>
+  const [messageApi, contextHolder] = message.useMessage();
+  useEffect(() => {
+    props.dispatch(
+      loadLanguages({
+        languages: {
+          en: require("./translations/english.json"),
+          fn: require("./translations/french.json"),
+          de: require("./translations/germany.json"),
+          vi: require("./translations/vietnames.json"),
+        },
+      })
     );
+  });
+  return (
+    <ToastProvider placement="bottom-left">
+      <BreadcrumbsProvider>
+        <Router>
+          <ScrollToTop messageApi={messageApi}>
+            <Suspense
+              fallback={
+                <div className="flone-preloader-wrapper">
+                  <div className="flone-preloader">
+                    <span></span>
+                    <span></span>
+                  </div>
+                </div>
+              }
+            >
+              {contextHolder}
+              <Switch>
+                <Route
+                  exact
+                  path={process.env.PUBLIC_URL + "/"}
+                  component={HomeOrganicFood}
+                />
+                {/* Homepages */}
+                <Route
+                  path={process.env.PUBLIC_URL + "/home-organic-food"}
+                  component={HomeOrganicFood}
+                />
+                {/* Shop pages */}
+                <Route
+                  path={process.env.PUBLIC_URL + "/shop-grid-standard"}
+                  component={ShopGridStandard}
+                />
+                {/* Shop product pages */}
+                <Route
+                  path={process.env.PUBLIC_URL + "/product/:id"}
+                  render={(routeProps) => (
+                    <Product {...routeProps} key={routeProps.match.params.id} />
+                  )}
+                />
+                {/* Blog pages */}
+                <Route
+                  path={process.env.PUBLIC_URL + "/blog-standard"}
+                  component={BlogStandard}
+                />
+                <Route
+                  path={process.env.PUBLIC_URL + "/blog-details-standard"}
+                  component={BlogDetailsStandard}
+                />
+                {/* Other pages */}
+                <Route
+                  path={process.env.PUBLIC_URL + "/about"}
+                  component={About}
+                />
+                <Route
+                  path={process.env.PUBLIC_URL + "/contact"}
+                  component={Contact}
+                />
+                <Route
+                  path={process.env.PUBLIC_URL + "/my-account"}
+                  component={MyAccount}
+                />
+                <Route
+                  path={process.env.PUBLIC_URL + "/login-register"}
+                  component={LoginRegister}
+                />
+                <Route
+                  path={process.env.PUBLIC_URL + "/cart"}
+                  component={Cart}
+                />
+                <Route
+                  path={process.env.PUBLIC_URL + "/wishlist"}
+                  component={Wishlist}
+                />
+                <Route
+                  path={process.env.PUBLIC_URL + "/compare"}
+                  component={Compare}
+                />
+                <Route
+                  path={process.env.PUBLIC_URL + "/checkout"}
+                  component={Checkout}
+                />
+                <Route
+                  path={process.env.PUBLIC_URL + "/Complete"}
+                  component={CompleteOrder}
+                />
+                <Route
+                  path={process.env.PUBLIC_URL + "/not-found"}
+                  component={NotFound}
+                />
+                {/* Router Admin Manager */}
+                <Route
+                  path="/admin"
+                  component={AdminLayout}
+                  render={() => <Redirect to="/admin/dashboard" />}
+                />
+                <Route exact component={NotFound} />
+              </Switch>
+            </Suspense>
+          </ScrollToTop>
+        </Router>
+      </BreadcrumbsProvider>
+    </ToastProvider>
+  );
 };
 
 App.propTypes = {
-    dispatch: PropTypes.func,
+  dispatch: PropTypes.func,
 };
 
 export default connect()(multilanguage(App));
