388815f096f13f0d3145270a23579542994c1733
giangtv83 <giangtv1712@gmail.com>
2023-07-12 21:50:31 +0700

    Biểu đồ Order

--- src/components/admin/StatisticsManager/CalculateOrder.jsx
+++ src/components/admin/StatisticsManager/CalculateOrder.jsx
@@ -1,71 +1,70 @@
 import React from 'react'
-import { Column } from '@ant-design/plots';
+import { Pie } from '@ant-design/plots';
 
 const CalculateOrder = () => {
     const data = [
         {
-            type: '家具家电',
-            sales: 38,
+            type: '分类一',
+            value: 27,
         },
         {
-            type: '粮油副食',
-            sales: 52,
+            type: '分类二',
+            value: 25,
         },
         {
-            type: '生鲜水果',
-            sales: 61,
+            type: '分类三',
+            value: 18,
         },
         {
-            type: '美容洗护',
-            sales: 145,
+            type: '分类四',
+            value: 15,
         },
         {
-            type: '母婴用品',
-            sales: 48,
+            type: '分类五',
+            value: 10,
         },
         {
-            type: '进口食品',
-            sales: 38,
-        },
-        {
-            type: '食品饮料',
-            sales: 38,
-        },
-        {
-            type: '家庭清洁',
-            sales: 38,
+            type: '其他',
+            value: 5,
         },
     ];
     const config = {
+        appendPadding: 10,
         data,
-        xField: 'type',
-        yField: 'sales',
+        angleField: 'value',
+        colorField: 'type',
+        radius: 1,
+        innerRadius: 0.6,
         label: {
-            // 可手动配置 label 数据标签位置
-            position: 'middle',
-            // 'top', 'bottom', 'middle',
-            // 配置样式
+            type: 'inner',
+            offset: '-50%',
+            content: '{value}',
             style: {
-                fill: '#FFFFFF',
-                opacity: 0.6,
+                textAlign: 'center',
+                fontSize: 14,
             },
         },
-        xAxis: {
-            label: {
-                autoHide: true,
-                autoRotate: false,
+        interactions: [
+            {
+                type: 'element-selected',
             },
-        },
-        meta: {
-            type: {
-                alias: '类别',
+            {
+                type: 'element-active',
             },
-            sales: {
-                alias: '销售额',
+        ],
+        statistic: {
+            title: false,
+            content: {
+                style: {
+                    whiteSpace: 'pre-wrap',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                },
+                content: 'Order',
             },
         },
     };
-    return <Column {...config} />;
+    return <Pie {...config} />;
 };
 
 export default CalculateOrder
