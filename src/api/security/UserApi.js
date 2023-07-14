3952aeedb33d5b72ec805349d80c791c97cd529e
giangtv83 <giangtv1712@gmail.com>
2023-07-14 22:38:33 +0700

    feat/UserEdit & SearchUser

--- src/api/security/UserApi.js
+++ src/api/security/UserApi.js
@@ -74,8 +74,19 @@ const UserApi = {
         return axiosClient.get(url, {
             headers: {
                 Authorization: `Bearer ${localStorage.getItem("token")
-                        ? JSON.parse(localStorage.getItem("token"))
-                        : ""
+                    ? JSON.parse(localStorage.getItem("token"))
+                    : ""
+                    }`,
+            },
+        });
+    },
+    updateUser: (id, body) => {
+        const url = `/User/updateUser/${id}`;
+        return axiosClient.post(url, body, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")
+                    ? JSON.parse(localStorage.getItem("token"))
+                    : ""
                     }`,
             },
         });
