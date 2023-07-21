0fa7c7d7e00d62dd34dcf9df8b5ebb761ea57f46
Dong Cong Dinh <dinhdc1111@gmail.com>
2023-07-21 22:07:21 +0700

    fix/Remove-the-excess - Edit text - Remove the excess on Product pages

--- src/components/product/ShopCategories.js
+++ src/components/product/ShopCategories.js
@@ -5,7 +5,7 @@ import { setActiveSort } from "../../helpers/product";
 const ShopCategories = ({ categories, getSortParams }) => {
   return (
     <div className="sidebar-widget">
-      <h4 className="pro-sidebar-title">Categories </h4>
+      <h4 className="pro-sidebar-title">Theo Danh Mục</h4>
       <div className="sidebar-widget-list mt-30">
         {categories ? (
           <ul>
@@ -17,7 +17,7 @@ const ShopCategories = ({ categories, getSortParams }) => {
                     setActiveSort(e);
                   }}
                 >
-                  <span className="checkmark" /> All Categories
+                  <span className="checkmark" /> Tất cả danh mục
                 </button>
               </div>
             </li>
--- src/components/product/ShopSearch.js
+++ src/components/product/ShopSearch.js
@@ -1,40 +1,41 @@
 import React from "react";
-import { useEffect } from "react";
 import { useState } from "react";
-
 const ShopSearch = ({ setFilterSortName }) => {
-  const [dataInput, setDataInput] = useState("");
-  const resetData = () => {
-    setFilterSortName("");
-    setDataInput("");
-  };
-  // useEffect(() => {}, [dataInput]);
-  return (
-    <div className="sidebar-widget">
-      <h4 className="pro-sidebar-title">Search </h4>
-      <div className="pro-sidebar-search mb-50 mt-25">
-        <form className="pro-sidebar-search-form" action="#">
-          <input
-            type="text"
-            placeholder="Search here..."
-            value={dataInput}
-            onChange={(e) => setDataInput(e.target.value)}
-          />
-          <button type="button" onClick={() => setFilterSortName(dataInput)}>
-            <i className="pe-7s-search" />
-          </button>
-        </form>
-        {dataInput.length > 0 && (
-          <button
-            className=" mb-50 mt-25"
-            type="button"
-            onClick={() => resetData()}>
-            làm mới ô tìm kiếm
-          </button>
-        )}
-      </div>
-    </div>
-  );
+    const [dataInput, setDataInput] = useState("");
+    const resetData = () => {
+        setFilterSortName("");
+        setDataInput("");
+    };
+    return (
+        <div className="sidebar-widget">
+            <h4 className="pro-sidebar-title">Tìm kiếm</h4>
+            <div className="pro-sidebar-search mb-50 mt-25">
+                <form className="pro-sidebar-search-form" action="#">
+                    <input
+                        type="text"
+                        placeholder="Tìm kiếm sản phẩm..."
+                        value={dataInput}
+                        onChange={(e) => setDataInput(e.target.value)}
+                    />
+                    <button
+                        type="button"
+                        onClick={() => setFilterSortName(dataInput)}
+                    >
+                        <i className="pe-7s-search" />
+                    </button>
+                </form>
+                {dataInput.length > 0 && (
+                    <button
+                        className=" mb-50 mt-25 btn btn-danger"
+                        type="button"
+                        onClick={() => resetData()}
+                    >
+                        Làm mới
+                    </button>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default ShopSearch;
