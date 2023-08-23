f50b784e3e1435b8997657ed8c4930646bf699ca
thuanlevan72 <88273694+thuanlevan72@users.noreply.github.com>
2023-08-24 03:43:32 +0700

    dsada

--- src/wrappers/product/TabProductFour.js
+++ src/wrappers/product/TabProductFour.js
@@ -37,61 +37,6 @@ const TabProductFour = ({
             </div>
           )}
         </div>
-        {/* <Tab.Container defaultActiveKey="bestSeller">
-          <Nav
-            variant="pills"
-            className={`product-tab-list pt-35 pb-60 text-center ${
-              productTabClass ? productTabClass : ""
-            }`}>
-            <Nav.Item>
-              <Nav.Link eventKey="newArrival">
-                <h4>Sản phẩm mới</h4>
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="bestSeller">
-                <h4>Bán chạy nhất</h4>
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="saleItems">
-                <h4>Mặt hàng bán</h4>
-              </Nav.Link>
-            </Nav.Item>
-          </Nav>
-          <Tab.Content>
-            <Tab.Pane eventKey="newArrival">
-              <div className="row">
-                <ProductGridTwo
-                  category={category}
-                  type="new"
-                  limit={8}
-                  spaceBottomClass="mb-25"
-                />
-              </div>
-            </Tab.Pane>
-            <Tab.Pane eventKey="bestSeller">
-              <div className="row">
-                <ProductGridTwo
-                  category={category}
-                  type="bestSeller"
-                  limit={8}
-                  spaceBottomClass="mb-25"
-                />
-              </div>
-            </Tab.Pane>
-            <Tab.Pane eventKey="saleItems">
-              <div className="row">
-                <ProductGridTwo
-                  category={category}
-                  type="saleItems"
-                  limit={8}
-                  spaceBottomClass="mb-25"
-                />
-              </div>
-            </Tab.Pane>
-          </Tab.Content>
-        </Tab.Container> */}
         <div className="view-more text-center mt-20 toggle-btn6 col-12">
           <Link
             className="loadMore6"
