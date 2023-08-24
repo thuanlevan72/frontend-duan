9f9c323261f9bebdd2c030d31053903f82721703
Dong Cong Dinh <dinhdc1111@gmail.com>
2023-08-24 12:54:41 +0700

    style: :art: Style Product Selling

--- src/components/product/ProductDescriptionInfo.js
+++ src/components/product/ProductDescriptionInfo.js
@@ -26,9 +26,6 @@ const ProductDescriptionInfo = ({
     const [selectedProductColor, setSelectedProductColor] = useState(
         product.variation ? product.variation[0].color : ""
     );
-    const [selectedProductSize, setSelectedProductSize] = useState(
-        product.variation ? product.variation[0].size[0].name : ""
-    );
     const [productStock, setProductStock] = useState(
         product.variation ? product.variation[0].size[0].stock : product.stock
     );
@@ -37,8 +34,6 @@ const ProductDescriptionInfo = ({
     const productCartQty = getProductCartQuantity(
         cartItems,
         product,
-        selectedProductColor,
-        selectedProductSize
     );
 
     return (
@@ -86,95 +81,6 @@ const ProductDescriptionInfo = ({
                 <p>{product.shortDescription}</p>
             </div>
 
-            {product.variation ? (
-                <div className="pro-details-size-color">
-                    <div className="pro-details-color-wrap">
-                        <span>Color</span>
-                        <div className="pro-details-color-content">
-                            {product.variation.map((single, key) => {
-                                return (
-                                    <label
-                                        className={`pro-details-color-content--single ${single.color}`}
-                                        key={key}
-                                    >
-                                        <input
-                                            type="radio"
-                                            value={single.color}
-                                            name="product-color"
-                                            checked={
-                                                single.color ===
-                                                    selectedProductColor
-                                                    ? "checked"
-                                                    : ""
-                                            }
-                                            onChange={() => {
-                                                setSelectedProductColor(
-                                                    single.color
-                                                );
-                                                setSelectedProductSize(
-                                                    single.size[0].name
-                                                );
-                                                setProductStock(
-                                                    single.size[0].stock
-                                                );
-                                                setQuantityCount(1);
-                                            }}
-                                        />
-                                        <span className="checkmark"></span>
-                                    </label>
-                                );
-                            })}
-                        </div>
-                    </div>
-                    <div className="pro-details-size">
-                        <span>Size</span>
-                        <div className="pro-details-size-content">
-                            {product.variation &&
-                                product.variation.map((single) => {
-                                    return single.color === selectedProductColor
-                                        ? single.size.map((singleSize, key) => {
-                                            return (
-                                                <label
-                                                    className={`pro-details-size-content--single`}
-                                                    key={key}
-                                                >
-                                                    <input
-                                                        type="radio"
-                                                        value={
-                                                            singleSize.name
-                                                        }
-                                                        checked={
-                                                            singleSize.name ===
-                                                                selectedProductSize
-                                                                ? "checked"
-                                                                : ""
-                                                        }
-                                                        onChange={() => {
-                                                            setSelectedProductSize(
-                                                                singleSize.name
-                                                            );
-                                                            setProductStock(
-                                                                singleSize.stock
-                                                            );
-                                                            setQuantityCount(
-                                                                1
-                                                            );
-                                                        }}
-                                                    />
-                                                    <span className="size-name">
-                                                        {singleSize.name}
-                                                    </span>
-                                                </label>
-                                            );
-                                        })
-                                        : "";
-                                })}
-                        </div>
-                    </div>
-                </div>
-            ) : (
-                ""
-            )}
             {product.affiliateLink ? (
                 <div className="pro-details-quality">
                     <div className="pro-details-cart btn-hover ml-0">
@@ -228,16 +134,14 @@ const ProductDescriptionInfo = ({
                                         product,
                                         addToast,
                                         quantityCount,
-                                        selectedProductColor,
-                                        selectedProductSize
                                     )
                                 }
                                 disabled={productCartQty >= productStock}
                             >
-                                Thêm
+                                Thêm vào giỏ
                             </button>
                         ) : (
-                            <button disabled>Hết hàng</button>
+                            <button disabled>Hết món</button>
                         )}
                     </div>
                     <div className="pro-details-wishlist">
@@ -322,36 +226,6 @@ const ProductDescriptionInfo = ({
             ) : (
                 ""
             )}
-
-            {/* <div className="pro-details-social">
-                <ul>
-                    <li>
-                        <a href="//facebook.com">
-                            <i className="fa fa-facebook" />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="//dribbble.com">
-                            <i className="fa fa-dribbble" />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="//pinterest.com">
-                            <i className="fa fa-pinterest-p" />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="//twitter.com">
-                            <i className="fa fa-twitter" />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="//linkedin.com">
-                            <i className="fa fa-linkedin" />
-                        </a>
-                    </li>
-                </ul>
-            </div> */}
         </div>
     );
 };
