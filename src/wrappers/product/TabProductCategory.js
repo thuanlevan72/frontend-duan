13461064d0800b84a6812e7c5591a622788207f4
thuanlevan72 <88273694+thuanlevan72@users.noreply.github.com>
2023-09-13 19:23:00 +0700

    feat: add data Category

--- src/wrappers/product/TabProductCategory.js
+++ src/wrappers/product/TabProductCategory.js
@@ -19,28 +19,28 @@ const TabProductCategory = ({
       productTypeId: 1,
       nameProductType: "Hàng chay cấp đông",
       imageTypeProduct:
-        "https://res.cloudinary.com/doedovklj/image/upload/v1686794187/xyz-abc_638224161851379056image.webp",
+        "https://res.cloudinary.com/doedovklj/image/upload/v1694237398/xyz-abc_638298593973418611image.png",
       createdAt: "2023-06-04T14:52:00.2831254",
       updatedAt: "2023-06-04T14:52:00.2831343",
-      key: "ffdbe0b0-c0ba-4f86-b040-6c3b530fccbc",
+      key: "add1d092-84c2-4c92-b105-ae8548a0bbe2",
     },
     {
       productTypeId: 3,
       nameProductType: "Món Cơm",
       imageTypeProduct:
-        "https://res.cloudinary.com/doedovklj/image/upload/v1686794050/xyz-abc_638224160489753108image.webp",
+        "https://res.cloudinary.com/doedovklj/image/upload/v1694237300/xyz-abc_638298592992322785image.png",
       createdAt: "2023-06-13T14:20:31.2382471",
       updatedAt: "2023-06-13T14:20:31.2382622",
-      key: "9748e660-c4c9-41a1-92db-e70b17503d05",
+      key: "92513a2e-0d14-40cc-8436-625d1da5fcfd",
     },
     {
       productTypeId: 4,
       nameProductType: "Món Nộm",
       imageTypeProduct:
-        "https://res.cloudinary.com/doedovklj/image/upload/v1686793993/xyz-abc_638224159912695779image.webp",
+        "https://res.cloudinary.com/doedovklj/image/upload/v1694237053/xyz-abc_638298590524334592image.png",
       createdAt: "2023-06-13T14:20:31.2382471",
       updatedAt: "2023-06-13T14:20:31.2382622",
-      key: "175874d9-ec95-44a8-abad-c35de3a37275",
+      key: "25ae9c9d-38c8-4be5-b0c9-0f613c2cd66e",
     },
     {
       productTypeId: 6,
@@ -49,13 +49,31 @@ const TabProductCategory = ({
         "https://res.cloudinary.com/doedovklj/image/upload/v1686793918/xyz-abc_638224159162019553image.webp",
       createdAt: "2023-06-13T14:20:31.2382471",
       updatedAt: "2023-06-13T14:20:31.2382622",
-      key: "5890ac64-1ad3-486e-b2b8-d4a4d0b1e821",
+      key: "1fb37a48-705d-44ad-af42-10b0b0965dde",
+    },
+    {
+      productTypeId: 47,
+      nameProductType: "Đồ Uống",
+      imageTypeProduct:
+        "https://res.cloudinary.com/doedovklj/image/upload/v1694237751/xyz-abc_638298597491440622image.png",
+      createdAt: "2023-09-09T12:35:52.0366667",
+      updatedAt: "0001-01-01T00:00:00",
+      key: "c6be60e1-293c-423a-86e5-5926bfce8c15",
+    },
+    {
+      productTypeId: 48,
+      nameProductType: "Rau Củ",
+      imageTypeProduct:
+        "https://res.cloudinary.com/doedovklj/image/upload/v1694244233/xyz-abc_638298662319140215image.png",
+      createdAt: "2023-09-09T14:23:54.18",
+      updatedAt: "0001-01-01T00:00:00",
+      key: "2e08e015-f39c-4abc-ba82-037a6bd5b95d",
     },
   ]);
 
   useEffect(async () => {
     try {
-      // const res = await categoryAPI.GetCategoryFrontend();
+      const res = await categoryAPI.GetCategoryFrontend();
       // setDatas(res);
     } catch (error) {}
   }, []);
